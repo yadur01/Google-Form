d4b6133c31b4090becf72620258f9e648f1d3e05
the_Odin_ studant_01 <yaduraj.workplace56@gmail.com>
2024-02-17 22:17:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const signInBtn = document.getElementById("signIn");
+const signUpBtn = document.getElementById("signUp");
+const fistForm = document.getElementById("form1");
+const secondForm = document.getElementById("form2");
+const container = document.querySelector(".container");
+
+signInBtn.addEventListener("click", () => {
+	container.classList.remove("right-panel-active");
+});
+
+signUpBtn.addEventListener("click", () => {
+	container.classList.add("right-panel-active");
+});
+
+fistForm.addEventListener("submit", (e) => e.preventDefault());
+secondForm.addEventListener("submit", (e) => e.preventDefault());
+
+function animateVideo() {
+    WebGLSampler.to("#mtvideo", {
+        duraction: 4;
+        repeat: -1;
+        yoyo: true;
+        scale: 1.2;
+    });
+}
+
+animateVideo();
